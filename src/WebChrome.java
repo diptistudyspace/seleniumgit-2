7c30bfb24aed38e2eb3ea984a85f735e5c15dac9
diptistudyspace <129668826+diptistudyspace@users.noreply.github.com>
2023-04-03 09:42:17 +0530

    Update WebChrome.java
    edited by me

--- src/WebChrome.java
+++ src/WebChrome.java
@@ -5,11 +5,13 @@ public class WebChrome {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dipti\\eclipse-workspace\\SeleniumWebDriver\\drivers\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\dipti\\eclipse-workspace\\SeleniumWebDriver\\drivers\\chromedriver.exe");
 		
-		WebDriver driver=new ChromeDriver();
+		//WebDriver driver=new ChromeDriver();
 		
-		driver.get("http://omayo.blogspot.com/");
+		//driver.get("http://omayo.blogspot.com/");
+		
+		system.out.println("Edited in github")
 		
 	
 
